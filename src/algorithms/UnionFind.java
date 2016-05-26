1a9f2ad5dee54e0ad8297f3ccecea5d0ac5b7266
Shun Gao <theodore.gao.1986@gmail.com>
2016-05-25 20:51:54 -0700

    1.5

---
+++ src/algorithms/UnionFind.java
@@ -0,0 +1,56 @@
+package algorithms;
+
+import java.io.IOException;
+
+import collections.StdRandom;
+
+public class UnionFind {
+    
+    private int count;
+    private int[] ids;
+    private int[] sizes;
+    
+    public UnionFind(int n) {
+        count = n;
+        ids = new int[n];
+        sizes = new int[n];
+        for (int i = 0; i < n; i++) { ids[i] = i; sizes[i] = 1; }
+    }
+    
+    public void union(int p, int q) {
+        int i = find(p);
+        int j = find(q);
+        if (i == j) return;
+        if (sizes[i] < sizes[j]) { ids[i] = j; sizes[j] += sizes[i]; }
+        else { ids[j] = i; sizes[i] += sizes[j]; }
+        count--;
+    }
+    
+    public int find(int p) {
+        if (p == ids[p]) return p;
+        else return ids[p] = find(ids[p]);
+    }
+    
+    boolean connected(int p, int q) {
+        return find(p) == find(q);
+    }
+    
+    public int count() {
+        return count;
+    }
+    
+    public static void main(String[] args) throws IOException {
+        int n = 1000000;
+        int k = 0;
+        UnionFind unionFind = new UnionFind(n);
+        while (true) {
+            k++;
+            int p = StdRandom.uniform(n);
+            int q = StdRandom.uniform(n);
+            unionFind.union(p, q);
+//            System.out.println("connect " + p + " " + q + " : " + unionFind.count);
+            if (unionFind.count == 1) break;
+        }
+        System.out.println(unionFind.count() + " components " + k);
+    }
+}
