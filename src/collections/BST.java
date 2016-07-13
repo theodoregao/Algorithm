90c9bd87c1dea59b5e23e146b803f63147624e7e
theodore.gao.1986@gmail.com <mdstwdh948>
2016-07-13 15:27:02 -0700

    test BST

--- src/collections/BST.java
+++ src/collections/BST.java
@@ -267,133 +267,128 @@ public class BST<Key extends Comparable<Key>, Value> implements SymbolTable<Key,
 //        System.out.println(max + ": " + st.get(max));
 //    }
     
-    // test floor & ceiling
-//    public static void main(String[] args) {
-//        SymbolTable<Integer, Integer> st = new BST<Integer, Integer>();
-//        st.put(1, 0);
-//        st.put(10, 1);
-//        st.put(100, 2);
-//  
-//        for (Integer key: st.keys())
-//            System.out.println(key + ": " + st.get(key));
-//
-//        System.out.println("floor");
-//        System.out.println(st.floor(0));
-//        System.out.println(st.floor(1));
-//        System.out.println(st.floor(9));
-//        System.out.println(st.floor(10));
-//        System.out.println(st.floor(99));
-//        System.out.println(st.floor(100));
-//        System.out.println(st.floor(101));
-//
-//        System.out.println("ceiling");
-//        System.out.println(st.ceiling(0));
-//        System.out.println(st.ceiling(1));
-//        System.out.println(st.ceiling(2));
-//        System.out.println(st.ceiling(9));
-//        System.out.println(st.ceiling(10));
-//        System.out.println(st.ceiling(11));
-//        System.out.println(st.ceiling(99));
-//        System.out.println(st.ceiling(100));
-//        System.out.println(st.ceiling(101));
-//    }
+    private static void testFloorCeiling() {
+        SymbolTable<Integer, Integer> st = new BST<Integer, Integer>();
+        st.put(1, 0);
+        st.put(10, 1);
+        st.put(100, 2);
+  
+        for (Integer key: st.keys())
+            System.out.println(key + ": " + st.get(key));
+
+        System.out.println("floor");
+        System.out.println(st.floor(0));
+        System.out.println(st.floor(1));
+        System.out.println(st.floor(9));
+        System.out.println(st.floor(10));
+        System.out.println(st.floor(99));
+        System.out.println(st.floor(100));
+        System.out.println(st.floor(101));
+
+        System.out.println("ceiling");
+        System.out.println(st.ceiling(0));
+        System.out.println(st.ceiling(1));
+        System.out.println(st.ceiling(2));
+        System.out.println(st.ceiling(9));
+        System.out.println(st.ceiling(10));
+        System.out.println(st.ceiling(11));
+        System.out.println(st.ceiling(99));
+        System.out.println(st.ceiling(100));
+        System.out.println(st.ceiling(101));
+    }
     
-//    public static void main(String[] args) {
-//        BST<String, Integer> bst = new BST<>();
-//        bst.put("C++", 0);
-//        bst.put("C++", 1);
-//        bst.put("Java", 0);
-//        bst.put("Python", 0);
-//        bst.put("C#", 0);
-//        bst.put("JavaScript", 0);
-//        bst.put("Ruby", 0);
-//        bst.put("Swift", 0);
-//        bst.put("Go", 0);
-//        
-//        bst.put("Python", 1);
-//        bst.put("Java", 1);
-//        bst.put("C#", 1);
-//        
-//        for (String key: bst.keys()) {
-//            System.out.println(key + ": " + bst.get(key));
-//        }
-//        
-//        // test size
-//        System.out.println("size: " + bst.size());
-//        System.out.println("height: " + bst.height());
-//        
-//        System.out.println(bst.min());
-//        System.out.println(bst.max());
-//        
-//        // test delete
-//        bst.delete("C++");
-//        
-//        // test size
-//        System.out.println("size: " + bst.size());
-//        System.out.println("height: " + bst.height());
-//        
-//        bst.delete("Python");
-//        
-//        // test size
-//        System.out.println("size: " + bst.size());
-//        System.out.println("height: " + bst.height());
-//
-//        bst.delete("C#");
-//        bst.delete("Swift");
-//        
-//        for (String key: bst.keys()) {
-//            System.out.println(key + ": " + bst.get(key));
-//        }
-//
-//        // test min, max
-//        for (String key: bst.keys()) {
-//            System.out.println(key + ": " + bst.get(key));
-//        }
-//
-//        System.out.println("min: " + bst.min());
-//        System.out.println("max: " + bst.max());
-//        
-//        // test size
-//        System.out.println("size: " + bst.size());
-//        System.out.println("height: " + bst.height());
-//        
-//        // test rank, select
-//        System.out.println("" + bst.rank("Gn"));
-//        System.out.println("" + bst.rank("Go"));
-//        System.out.println("" + bst.rank("Gp"));
-//        System.out.println("" + bst.rank("Jav"));
-//        System.out.println("" + bst.rank("Java"));
-//        System.out.println("" + bst.rank("JavaA"));
-//        System.out.println("" + bst.rank("Rubx"));
-//        System.out.println("" + bst.rank("Ruby"));
-//        System.out.println("" + bst.rank("Rubz"));
-//        for (int i = 0; i < bst.size(); i++)
-//            System.out.println(bst.select(i));
-//        
-//        // test floor, ceiling
-//        System.out.println("" + bst.floor("Gn"));
-//        System.out.println("" + bst.floor("Go"));
-//        System.out.println("" + bst.floor("Gp"));
-//        System.out.println("" + bst.floor("Jav"));
-//        System.out.println("" + bst.floor("Java"));
-//        System.out.println("" + bst.floor("JavaA"));
-//        System.out.println("" + bst.floor("Rubx"));
-//        System.out.println("" + bst.floor("Ruby"));
-//        System.out.println("" + bst.floor("Rubz"));
-//
-//        System.out.println("" + bst.ceiling("Gn"));
-//        System.out.println("" + bst.ceiling("Go"));
-//        System.out.println("" + bst.ceiling("Gp"));
-//        System.out.println("" + bst.ceiling("Jav"));
-//        System.out.println("" + bst.ceiling("Java"));
-//        System.out.println("" + bst.ceiling("JavaA"));
-//        System.out.println("" + bst.ceiling("Rubx"));
-//        System.out.println("" + bst.ceiling("Ruby"));
-//        System.out.println("" + bst.ceiling("Rubz"));
-//    }
-    
-    public boolean isBinaryTree() {
-        return isBinaryTree(root);
+    private static void testBasic() {
+        BST<String, Integer> bst = new BST<>();
+        bst.put("C++", 0);
+        bst.put("C++", 1);
+        bst.put("Java", 0);
+        bst.put("Python", 0);
+        bst.put("C#", 0);
+        bst.put("JavaScript", 0);
+        bst.put("Ruby", 0);
+        bst.put("Swift", 0);
+        bst.put("Go", 0);
+        
+        bst.put("Python", 1);
+        bst.put("Java", 1);
+        bst.put("C#", 1);
+        
+        for (String key: bst.keys()) {
+            System.out.println(key + ": " + bst.get(key));
+        }
+        
+        // test size
+        System.out.println("size: " + bst.size());
+        System.out.println("height: " + bst.height());
+        
+        System.out.println(bst.min());
+        System.out.println(bst.max());
+        
+        // test delete
+        bst.delete("C++");
+        
+        // test size
+        System.out.println("size: " + bst.size());
+        System.out.println("height: " + bst.height());
+        
+        bst.delete("Python");
+        
+        // test size
+        System.out.println("size: " + bst.size());
+        System.out.println("height: " + bst.height());
+
+        bst.delete("C#");
+        bst.delete("Swift");
+        
+        for (String key: bst.keys()) {
+            System.out.println(key + ": " + bst.get(key));
+        }
+
+        // test min, max
+        for (String key: bst.keys()) {
+            System.out.println(key + ": " + bst.get(key));
+        }
+
+        System.out.println("min: " + bst.min());
+        System.out.println("max: " + bst.max());
+        
+        // test size
+        System.out.println("size: " + bst.size());
+        System.out.println("height: " + bst.height());
+        
+        // test rank, select
+        System.out.println("" + bst.rank("Gn"));
+        System.out.println("" + bst.rank("Go"));
+        System.out.println("" + bst.rank("Gp"));
+        System.out.println("" + bst.rank("Jav"));
+        System.out.println("" + bst.rank("Java"));
+        System.out.println("" + bst.rank("JavaA"));
+        System.out.println("" + bst.rank("Rubx"));
+        System.out.println("" + bst.rank("Ruby"));
+        System.out.println("" + bst.rank("Rubz"));
+        for (int i = 0; i < bst.size(); i++)
+            System.out.println(bst.select(i));
+        
+        // test floor, ceiling
+        System.out.println("" + bst.floor("Gn"));
+        System.out.println("" + bst.floor("Go"));
+        System.out.println("" + bst.floor("Gp"));
+        System.out.println("" + bst.floor("Jav"));
+        System.out.println("" + bst.floor("Java"));
+        System.out.println("" + bst.floor("JavaA"));
+        System.out.println("" + bst.floor("Rubx"));
+        System.out.println("" + bst.floor("Ruby"));
+        System.out.println("" + bst.floor("Rubz"));
+
+        System.out.println("" + bst.ceiling("Gn"));
+        System.out.println("" + bst.ceiling("Go"));
+        System.out.println("" + bst.ceiling("Gp"));
+        System.out.println("" + bst.ceiling("Jav"));
+        System.out.println("" + bst.ceiling("Java"));
+        System.out.println("" + bst.ceiling("JavaA"));
+        System.out.println("" + bst.ceiling("Rubx"));
+        System.out.println("" + bst.ceiling("Ruby"));
+        System.out.println("" + bst.ceiling("Rubz"));
     }
     
     private boolean isBinaryTree(Node node) {
@@ -410,7 +405,54 @@ public class BST<Key extends Comparable<Key>, Value> implements SymbolTable<Key,
         else return Math.max(calculateHeight(node.left), calculateHeight(node.right)) + 1;
     }
     
-    public static void main(String[] args) {
+    private Key preKey;
+    
+    private boolean isOrdered(Node node, Key min, Key max) {
+        preKey = null;
+        return isOrderedInternal(node, min, max);
+    }
+    
+    private boolean isOrderedInternal(Node node, Key min, Key max) {
+        return node == null ||
+                (isOrderedInternal(node.left, min, max)
+                        && checkNodeOrdered(node, min, max)
+                        && isOrderedInternal(node.right, min, max));
+    }
+    
+    private boolean checkNodeOrdered(Node node, Key min, Key max) {
+        boolean isOrdered = (preKey == null || preKey.compareTo(node.key) <= 0)
+                && min.compareTo(node.key) <= 0 && node.key.compareTo(max) <= 0;
+        preKey = node.key;
+//        System.out.println("update preKey to: " + preKey);
+        return isOrdered;
+    }
+    
+    private boolean hasNoDuplicates(Node node) {
+        preKey = null;
+        return hasNoDuplicatesInternal(node);
+    }
+    
+    private boolean hasNoDuplicatesInternal(Node node) {
+        return node == null ||
+                (hasNoDuplicatesInternal(node.left)
+                        && checkNoDuplicates(node)
+                        && hasNoDuplicatesInternal(node.right));
+    }
+    
+    private boolean checkNoDuplicates(Node node) {
+        boolean isNoDuplicates = preKey == null || preKey.compareTo(node.key) != 0;
+        preKey = node.key;
+//        System.out.println("update preKey to: " + preKey);
+        return isNoDuplicates;
+    }
+    
+    private boolean isBST() {
+        return isBinaryTree(root)
+                && isOrdered(root, min(), max())
+                && hasNoDuplicates(root);
+    }
+    
+    private static void testBst() {
         BST<String, Integer> bst = new BST<>();
         bst.put("C++", 0);
         bst.put("C++", 1);
@@ -426,11 +468,40 @@ public class BST<Key extends Comparable<Key>, Value> implements SymbolTable<Key,
         bst.put("Java", 1);
         bst.put("C#", 1);
         
-        System.out.println(bst.root);
-        System.out.println(bst.isBinaryTree(bst.root));
-        System.out.println(bst.root.left);
-        System.out.println(bst.isBinaryTree(bst.root.left));
-        System.out.println(bst.root.right);
-        System.out.println(bst.isBinaryTree(bst.root.right));
+        System.out.println(bst.isBST());
+    }
+    
+    private static void testSelectRank() {
+        BST<String, Integer> bst = new BST<>();
+        bst.put("C++", 0);
+        bst.put("C++", 1);
+        bst.put("Java", 0);
+        bst.put("Python", 0);
+        bst.put("C#", 0);
+        bst.put("JavaScript", 0);
+        bst.put("Ruby", 0);
+        bst.put("Swift", 0);
+        bst.put("Go", 0);
+      
+        bst.put("Python", 1);
+        bst.put("Java", 1);
+        bst.put("C#", 1);
+        
+        int i = 0;
+        for (String key: bst.keys()) {
+            System.out.println(key);
+            System.out.println(i);
+            System.out.println(bst.select(i++));
+            System.out.println(bst.rank(key));
+            System.out.println();
+        }
+    }
+    
+    // test rank, select
+    public static void main(String[] args) {
+//        testFloorCeiling();
+//        testBasic();
+//        testBst();
+        testSelectRank();
     }
 }
