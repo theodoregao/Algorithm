593801d8bdd71a32e5438307ac30d8a41b603afe
Shun Gao <theodore.gao.1986@gmail.com>
2018-04-08 16:47:33 -0700

    add binary search.

---
+++ src/problems/algotoolbox/week4/BinarySearch.java
@@ -0,0 +1,74 @@
+package problems.algotoolbox.week4;
+
+
+import java.io.*;
+import java.util.*;
+
+public class BinarySearch {
+
+    static int binarySearch(int[] a, int x) {
+        int left = 0, right = a.length - 1;
+        while (left <= right) {
+            int mid = (left + right) >>> 1;
+            if (x == a[mid]) return mid;
+            else if (x > a[mid]) left = mid + 1;
+            else right = mid - 1;
+        }
+        return -1;
+    }
+
+    static int linearSearch(int[] a, int x) {
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == x) return i;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        FastScanner scanner = new FastScanner(System.in);
+        int n = scanner.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+        int m = scanner.nextInt();
+        int[] b = new int[m];
+        for (int i = 0; i < m; i++) {
+          b[i] = scanner.nextInt();
+        }
+        for (int i = 0; i < m; i++) {
+            //replace with the call to binarySearch when implemented
+            System.out.print(binarySearch(a, b[i]) + " ");
+        }
+        
+//        for (int x: new int[] {8, 1, 23, 1, 11})
+//            System.out.println(binarySearch(new int[] {1, 5, 8, 12, 13}, x));
+    }
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastScanner(InputStream stream) {
+            try {
+                br = new BufferedReader(new InputStreamReader(stream));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
